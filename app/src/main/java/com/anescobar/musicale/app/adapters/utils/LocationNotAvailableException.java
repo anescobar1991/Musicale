9ab641fae5708f3e93bfad1d4b0f8c470dadeeb5
Andres Escobar <anescobar1991@gmail.com>
2014-11-23 21:09:17 -0700

    Moved files around for better organization

--- app/src/main/java/com/anescobar/musicale/app/adapters/utils/LocationNotAvailableException.java
+++
@@ -1,9 +0,0 @@
-package com.anescobar.musicale.app.adapters.utils;
-
-public class LocationNotAvailableException extends Exception {
-
-    public LocationNotAvailableException(String message){
-        super(message);
-    }
-
-}
